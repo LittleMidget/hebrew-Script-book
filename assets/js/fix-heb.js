7642b1634d5814fe6b596855741050d9bbb4588a
Harel Herer <harel585@gmail.com>
2020-01-22 23:02:29 +0200

    heb fix

---
+++ assets/js/fix-heb.js
@@ -0,0 +1,8 @@
+//document.getElementById("#slider").style.property += "display: flex;flex-direction: row-reverse;";
+
+   $(window).on('load', function() {
+    var style = $('#slider').attr('style');
+    style += "display: flex;flex-direction: row-reverse;";
+    $('#slider').attr('style', style);
+			setTimeout(function(){nav.last();},150);
+   });
