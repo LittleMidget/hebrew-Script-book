312372c0f5204a9eaa9516a07400b457e413d432
Harel Herer <harel585@gmail.com>
2020-02-06 13:28:43 +0200

    add interactions and new arrows

--- assets/js/fix-heb.js
+++ assets/js/fix-heb.js
@@ -1,12 +1,11 @@
 //document.getElementById("#slider").style.property += "display: flex;flex-direction: row-reverse;";
 
-   $(window).on('load', function() {
-    var style = $('#slider').attr('style');
-    style += "display: flex;flex-direction: row-reverse;";
-    $('#slider').attr('style', style);
-			setTimeout(function(){nav.last();},150);
-   });
+
    
    function goRight(){
 	   nav.next();
+   }
+   
+      function goLeft(){
+	   nav.back();
    }
